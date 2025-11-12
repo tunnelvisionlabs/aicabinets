fc101bb0bba4d71338359f0808cd9c0e78d4c31e
Sam Harwell <sam@tunnelvisionlabs.com>
2025-11-12 10:46:15 -0800

    compat: remove undo shim

--- aicabinets/loader.rb
+++ aicabinets/loader.rb
@@ -2,7 +2,6 @@
 
 module AICabinets
   if defined?(Sketchup)
-    Sketchup.require('aicabinets/compat/undo')
     Sketchup.require('aicabinets/features')
     Sketchup.require('aicabinets/version')
     Sketchup.require('aicabinets/ops/units')
