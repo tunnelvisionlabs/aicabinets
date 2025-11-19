f4143f869c2a3fe325dfdcaed9b2816ff094f8c1
Sam Harwell <sam@tunnelvisionlabs.com>
2025-11-19 14:51:02 -0800

    chore: fix rubocop lint for unit test runner

--- script/run_unit_tests.rb
+++ script/run_unit_tests.rb
@@ -4,7 +4,7 @@
 root = File.expand_path('..', __dir__)
 $LOAD_PATH.unshift(root)
 
-test_files = Dir[File.join(root, 'test', 'unit', 'test_*.rb')].sort
+test_files = Dir[File.join(root, 'test', 'unit', 'test_*.rb')]
 abort('No unit tests found under test/unit') if test_files.empty?
 
 test_files.each do |file|
